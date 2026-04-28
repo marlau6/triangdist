e412ad415f666b4ebf7e2da0df0ae95361ade88c
marlau6 <marlau6@alumni.uv.es>
2026-04-28 19:12:04 +0200

    tested ptriang function

--- tests/testthat/test-triangular.R
+++ tests/testthat/test-triangular.R
@@ -2,7 +2,7 @@ library(testthat)
 library(triangdist)
 
 test_that("dtriang: parameter validation and error messages", {
-  # These tests ensure 100% coverage of your stop() conditions
+  # Coverage of the stop() conditions
 
   e1 <- "Invalid parameters: min > max"
   e2 <- "Invalid parameters: mode < min"
@@ -42,4 +42,50 @@ test_that("dtriang: vectorization and recycling rules", {
 
   # Parameter recycling (vectorized min)
   expect_length(dtriang(5, min = c(0, 0), max = 10, mode = 5), 2)
+})
+
+test_that("ptriang: parameter validation and error messages", {
+  # Coverage of the stop() conditions
+
+  e1 <- "Invalid parameters: min > max"
+  e2 <- "Invalid parameters: mode < min"
+  e3 <- "Invalid parameters: mode > max"
+
+  expect_error(ptriang(0.5, min = 10, max = 5, mode = 7), e1)
+  expect_error(ptriang(0.5, min = 2, max = 10, mode = 1), e2)
+  expect_error(ptriang(0.5, min = 0, max = 5, mode = 6), e3)
+})
+
+test_that("ptriang: cumulative distribution values for different segments", {
+  # Scenario: a=0, b=10, c=2
+  a <- 0
+  b <- 10
+  c <- 2
+
+  # 1. Below 'min' (initializes Y as 0)
+  expect_equal(ptriang(-1, a, b, c), 0)
+
+  # 2. Case 'a_to_c': q = 1
+  expect_equal(ptriang(1, a, b, c), 0.05)
+
+  # 3. Case 'x_eq_c': q = 2 (The Mode)
+  expect_equal(ptriang(2, a, b, c), 0.2)
+
+  # 4. Case 'c_to_b': q = 6
+  expect_equal(ptriang(6, a, b, c), 0.8)
+
+  # 5. Case 'x_bgr_b': q = 11 (Biger than max)
+  expect_equal(ptriang(11, a, b, c), 1)
+})
+
+test_that("ptriang: vectorization and recycling rules", {
+  # Input vector
+  q_vec <- c(1, 2, 6)
+  expected_res <- c(0.05, 0.2, 0.8)
+
+  expect_length(ptriang(q_vec, 0, 10, 2), 3)
+  expect_equal(ptriang(q_vec, 0, 10, 2), expected_res)
+
+  # Parameter recycling (vectorized min)
+  expect_length(ptriang(0.5, min = c(0, 0), max = 1, mode = 0.5), 2)
 })
